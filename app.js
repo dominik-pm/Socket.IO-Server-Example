7fa983dd3507a7e9bc8e7e1e7d6bace87f04db24
Dominik Pömmer <46850095+dominik-pm@users.noreply.github.com>
2019-05-30 13:08:32 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,24 @@
+var express = require("express");
+var app = express();
+var server = require("http").Server(app);
+
+app.get("/", function(req, res)
+{
+    res.sendFile(__dirname + "/client/index.html");
+});
+app.use("/client", express.static(__dirname + "/client"));
+
+var port = 80;
+server.listen(port);
+console.log("Server started. Port = " + port);
+
+var io = require("socket.io")(server, {});
+
+io.sockets.on("connection", function(socket)
+{
+    console.log("socket connection");
+
+    socket.on('connectionSuccessful', function(msg) {
+        console.log(socket.id + ' just sent a message: ' + msg);
+    });
+});
